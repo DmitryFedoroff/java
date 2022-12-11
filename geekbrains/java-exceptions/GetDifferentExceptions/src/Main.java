ec1cb54345d63c400018132d3ab09d8409814d8f
DmitryFedoroff <fedoroffx@gmail.com>
2022-12-11 23:27:25 +0700

    feat: Assign random integer for each element of array

--- geekbrains/java-exceptions/GetDifferentExceptions/src/Main.java
+++ geekbrains/java-exceptions/GetDifferentExceptions/src/Main.java
@@ -1,7 +1,20 @@
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args) {
 
 		String[] strArr = { "Text printed before exception is thrown", null, "Text will not be printed" };
+	}
+
+	public static int[] fillArray(int len) {
+
+		int[] array = new int[len];
+		Random rand = new Random();
+
+		for (int i = 0; i < len; i++) {
+			array[i] = rand.nextInt(1, 10);
+		}
+		return array;
 	}
 }
