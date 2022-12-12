9703f68556fdfbeab17fb672cd28a114e1e48c94
DmitryFedoroff <fedoroffx@gmail.com>
2022-12-12 23:45:45 +0700

    feat: Find difference between elements of two arrays of integers and throw exception if lengths of arrays are not equal or one of incomming arrays is null

--- geekbrains/java-exceptions/CheckArraysAreEqualOrNull/src/Main.java
+++ geekbrains/java-exceptions/CheckArraysAreEqualOrNull/src/Main.java
@@ -9,6 +9,24 @@ public class Main {
         int[] secondArray = fillArray(9);
     }
 
+    public static int[] diffArrays(int[] firstArr, int[] secondArr) {
+
+        if (firstArr == null || secondArr == null) {
+            throw new RuntimeException("One of incoming arrays is null.");
+        }
+
+        if (firstArr.length != secondArr.length) {
+            throw new RuntimeException("Arrays have different lengths.");
+        }
+
+        int[] array = new int[firstArr.length];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = firstArr[i] - secondArr[i];
+        }
+        return array;
+    }
+
     public static int[] fillArray(int len) {
 
         int[] array = new int[len];
