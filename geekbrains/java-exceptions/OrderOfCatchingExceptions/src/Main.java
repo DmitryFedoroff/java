97ea9b9683cc4e3be791bbdf185be69d043c9f54
DmitryFedoroff <fedoroffx@gmail.com>
2022-12-17 20:36:18 +0700

    refactor: Change order of exceptions to catch most specific first and then most generic exceptions

--- geekbrains/java-exceptions/OrderOfCatchingExceptions/src/Main.java
+++ geekbrains/java-exceptions/OrderOfCatchingExceptions/src/Main.java
@@ -6,5 +6,16 @@ public class Main {
         abc[3] = 9;
         int a = 90;
         int b = 3;
+
+        try {
+            System.out.println(a / b);
+            printSum(23, 234);
+        } catch (NullPointerException ex) {
+            System.out.println("Attempt to access reference variable that currently points to null.");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Array accessed with illegal index.");
+        } catch (Throwable ex) {
+            System.out.println("Something went wrong...");
+        }
     }
 }
