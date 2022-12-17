44ede5c1ca063d0f2a8852395799b7887e5e1de8
DmitryFedoroff <fedoroffx@gmail.com>
2022-12-17 20:42:12 +0700

    feat: Sum two numbers and print their sum

--- geekbrains/java-exceptions/OrderOfCatchingExceptions/src/Main.java
+++ geekbrains/java-exceptions/OrderOfCatchingExceptions/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] abc = { 1, 2 };
+        int[] abc = {1, 2};
         abc[3] = 9;
         int a = 90;
         int b = 3;
@@ -17,5 +17,9 @@ public class Main {
         } catch (Throwable ex) {
             System.out.println("Something went wrong...");
         }
+    }
+
+    public static void printSum(Integer a, Integer b) {
+        System.out.println(a + b);
     }
 }
