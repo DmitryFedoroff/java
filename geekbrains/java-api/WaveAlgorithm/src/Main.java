7457f1b60553e58432621b495eec38faa65b0dea
DmitryFedoroff <fedoroffx@gmail.com>
2023-01-01 17:39:21 +0700

    dc: Create temp matrix and start cell

--- geekbrains/java-api/WaveAlgorithm/src/Main.java
+++ geekbrains/java-api/WaveAlgorithm/src/Main.java
@@ -16,5 +16,10 @@ public class Main {
                 { 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1 },
                 { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
         };
+
+        /* Create temp matrix for necessary calculations */
+        int start = 1;
+        int[][] mazeTmp = new int[maze.length][maze[0].length];
+        mazeTmp[start][start] = 1;
     }
 }
