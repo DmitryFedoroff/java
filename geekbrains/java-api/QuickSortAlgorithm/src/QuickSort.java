94fb3b3907b2cb7fe7f0cd970df399617c684032
DmitryFedoroff <fedoroffx@gmail.com>
2023-01-03 16:19:41 +0700

    feat: Exchange two specific elements in defined positions

---
+++ geekbrains/java-api/QuickSortAlgorithm/src/QuickSort.java
@@ -0,0 +1,9 @@
+public class QuickSort {
+
+    static void swap(int[] arr, int low, int pivot) {
+
+        int temp = arr[low];
+        arr[low] = arr[pivot];
+        arr[pivot] = temp;
+    }
+}
