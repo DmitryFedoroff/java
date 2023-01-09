1907bef7af4eb02d05f4289281fee6fa0216e68e
DmitryFedoroff <fedoroffx@gmail.com>
2023-01-09 20:28:00 +0700

    style: Remove blank line right after any method signature in Main class

--- geekbrains/java-api/CheckPalindrome/src/Main.java
+++ geekbrains/java-api/CheckPalindrome/src/Main.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
         String myStr = validateStrInput();
+
         System.out.print(PalindromeValid.isPalindrome(myStr));
     }
 
     public static String validateStrInput() {
-
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Enter palindrome: ");
