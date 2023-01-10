242e91e0ba77d6d8913c55d43a4e325f2264e471
DmitryFedoroff <fedoroffx@gmail.com>
2023-01-10 18:48:59 +0700

    dc: Initialize variables

---
+++ geekbrains/java-api/MultiplyMatrices/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Enter number of matrix rows: ");
+        int n = validateIntInput();
+        System.out.print("Enter number of matrix columns: ");
+        int m = validateIntInput();
+    }
+}
