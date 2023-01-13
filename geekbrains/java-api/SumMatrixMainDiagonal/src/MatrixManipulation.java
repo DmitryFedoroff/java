be488e60cbd865b69dbe6d2dc08f616d55205cbb
DmitryFedoroff <fedoroffx@gmail.com>
2023-01-13 22:25:32 +0700

    feat: Create matrix of user-defined size and assign random integers generated within specific range for each element of matrix

---
+++ geekbrains/java-api/SumMatrixMainDiagonal/src/MatrixManipulation.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class MatrixManipulation {
+
+    public static int[][] create(int rows, int columns) {
+        int[][] matr = new int[rows][columns];
+        int min = 0;
+        int max = 10;
+        Random rand = new Random();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                matr[i][j] = rand.nextInt(max - min + 1) + min;
+            }
+        }
+        return matr;
+    }
+}
