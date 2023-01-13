4168cbb87533a6ab4cd043401995b3e28d45780a
DmitryFedoroff <fedoroffx@gmail.com>
2023-01-13 22:35:50 +0700

    feat: Print sum of elements on matrix main diagonal

--- geekbrains/java-api/SumMatrixMainDiagonal/src/MatrixManipulation.java
+++ geekbrains/java-api/SumMatrixMainDiagonal/src/MatrixManipulation.java
@@ -2,6 +2,16 @@ import java.util.Random;
 
 public class MatrixManipulation {
 
+    public static void sumMainDiag(int[][] matr) {
+        int sum = 0;
+        int minLen = Math.min(matr.length, matr[0].length);
+
+        for (int i = 0; i < minLen; i++) {
+            sum += matr[i][i];
+        }
+        System.out.print("Sum of elements on matrix main diagonal: " + sum);
+    }
+
     public static int[][] create(int rows, int columns) {
         int[][] matr = new int[rows][columns];
         int min = 0;
