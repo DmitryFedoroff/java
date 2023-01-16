358f2a7c9ec70b952a6fdd3f95eba1ed02f50be2
DmitryFedoroff <fedoroffx@gmail.com>
2023-01-16 21:10:05 +0700

    feat: Sort elements of array using wavesort algorithm

--- geekbrains/java-api/SortArrayInWaveForm/src/WaveSort.java
+++ geekbrains/java-api/SortArrayInWaveForm/src/WaveSort.java
@@ -1,5 +1,18 @@
 public class WaveSort {
 
+    public static void sort(int[] arr) {
+        int len = arr.length;
+
+        for (int i = 0; i < len; i+= 2) {
+            if (i > 0 && arr[i - 1] > arr[i]) {
+                swap(arr, i - 1, i);
+            }
+            if (i < len - 1 && arr[i] < arr[i + 1]) {
+                swap(arr, i, i + 1);
+            }
+        }
+    }
+
     static void swap(int[] arr, int a, int b) {
         int temp = arr[a];
         arr[a] = arr[b];
