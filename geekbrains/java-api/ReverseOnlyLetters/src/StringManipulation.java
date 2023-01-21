ba9163e588fd9a4a0a03d3f271d56c59b5e7560a
DmitryFedoroff <fedoroffx@gmail.com>
2023-01-21 18:44:55 +0700

    feat: Exchange two specific elements in defined positions

---
+++ geekbrains/java-api/ReverseOnlyLetters/src/StringManipulation.java
@@ -0,0 +1,8 @@
+public class StringManipulation {
+
+    static void swap(char[] chars, int a, int b) {
+        char temp = chars[a];
+        chars[a] = chars[b];
+        chars[b] = temp;
+    }
+}
