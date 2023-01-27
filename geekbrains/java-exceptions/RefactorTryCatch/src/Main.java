347fb9d4b7ae66dfd68b39115f3afa6dadcfec48
DmitryFedoroff <fedoroffx@gmail.com>
2023-01-27 21:25:43 +0700

    style: Remove blank line right after method signature in Main class

--- geekbrains/java-exceptions/RefactorTryCatch/src/Main.java
+++ geekbrains/java-exceptions/RefactorTryCatch/src/Main.java
@@ -1,21 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
-
         int[] intArray = { 0, 1, 2, 3, 4, 5, 6, 7 };
         int index = 8;
         int d = 0;
 
         if (intArray == null) {
-            throw new RuntimeException("Array is not instantiated or points to null reference.");
+            throw new RuntimeException("Array is not instantiated or points to null reference");
         }
 
         if (index > intArray.length - 1) {
-            throw new ArrayIndexOutOfBoundsException("Index " + index + " is greater than or equal to size of array.");
+            throw new ArrayIndexOutOfBoundsException("Index " + index + " is greater than or equal to size of array");
         }
 
         if (d == 0) {
-            throw new ArithmeticException("You can't divide by zero.");
+            throw new ArithmeticException("You can't divide by zero");
         }
 
         double catchedRes1 = intArray[index] / d;
