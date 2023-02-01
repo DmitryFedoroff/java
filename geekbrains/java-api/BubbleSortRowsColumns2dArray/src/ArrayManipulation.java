52f470b3c2df26cc123e3d1ea9dca4671ed828f0
DmitryFedoroff <fedoroffx@gmail.com>
2023-02-01 20:38:52 +0700

    feat: Sort elements of each row of two-dimensional array using bubble sort

--- geekbrains/java-api/BubbleSortRowsColumns2dArray/src/ArrayManipulation.java
+++ geekbrains/java-api/BubbleSortRowsColumns2dArray/src/ArrayManipulation.java
@@ -2,6 +2,23 @@ import java.util.Random;
 
 public class ArrayManipulation {
 
+    public static void bubbleSortRow(int[][] arr) {
+        int rowsLen = arr.length;
+        int columnsLen = arr[0].length;
+
+        for (int i = 0; i < rowsLen; i++) {
+            for (int j = 0; j < columnsLen; j++) {
+                for (int k = j + 1; k < columnsLen; k++) {
+                    if (arr[i][j] < arr[i][k]) {
+                        int temp = arr[i][j];
+                        arr[i][j] = arr[i][k];
+                        arr[i][k] = temp;
+                    }
+                }
+            }
+        }
+    }
+
     public static int[][] create(int rowsLen, int columnsLen) {
         int[][] arr = new int[rowsLen][columnsLen];
         int min = 0;
