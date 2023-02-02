d6ab6e6c7f26b9fbd4404fddd0de01cd9a21191c
DmitryFedoroff <fedoroffx@gmail.com>
2023-02-02 20:02:49 +0700

    dc: Create array of user-defined size

--- geekbrains/java-api/FibonacciWithoutRecursion/src/Main.java
+++ geekbrains/java-api/FibonacciWithoutRecursion/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Enter length of Fibonacci sequence: ");
         int n = validateIntInput();
+        int[] array = new int[n];
     }
 
     public static int validateIntInput() {
