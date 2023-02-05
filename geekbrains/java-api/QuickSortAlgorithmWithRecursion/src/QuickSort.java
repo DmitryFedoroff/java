0e8a38d5ba1d9677afad8094e72ea2390f25c964
DmitryFedoroff <fedoroffx@gmail.com>
2023-02-05 20:11:11 +0700

    feat: Return index at which pivot element is placed in array

--- geekbrains/java-api/QuickSortAlgorithmWithRecursion/src/QuickSort.java
+++ geekbrains/java-api/QuickSortAlgorithmWithRecursion/src/QuickSort.java
@@ -1,5 +1,19 @@
 public class QuickSort {
 
+    static int getPivotIndex(int[] arr, int minIdx, int maxIdx) {
+        int pivotIdx = minIdx - 1;
+
+        for (int i = minIdx; i < maxIdx; i++) {
+            if (arr[i] < arr[maxIdx]) {
+                pivotIdx++;
+                swap(arr, i, pivotIdx);
+            }
+        }
+        pivotIdx++;
+        swap(arr, pivotIdx, maxIdx);
+        return pivotIdx;
+    }
+
     static void swap(int[] array, int leftValue, int rightValue) {
         int temp = array[leftValue];
         array[leftValue] = array[rightValue];
