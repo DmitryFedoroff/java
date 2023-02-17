93545691499592b486b7d18b62ea1ff7578d96c2
DmitryFedoroff <fedoroffx@gmail.com>
2023-02-17 23:13:25 +0700

    dc: Add calculateTriangNum method to print triangular number

--- geekbrains/java-api/FindTriangularNumber/src/Main.java
+++ geekbrains/java-api/FindTriangularNumber/src/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Enter natural number to find its triangular number: ");
         int n = validateIntInput();
+        TriangularNum.calculateTriangNum(n);
+
         System.out.print("Enter triangular number to check: ");
         int m = validateIntInput();
     }
