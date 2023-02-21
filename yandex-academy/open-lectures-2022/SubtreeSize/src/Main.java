9f53833a643e11d785f43a8319a3333378140f82
DmitryFedoroff <fedoroffx@gmail.com>
2023-02-21 23:40:32 +0700

    feat: Check if input is positive integer and ask user to reenter number if it is not or throw IllegalArgumentException if input is empty

--- yandex-academy/open-lectures-2022/SubtreeSize/src/Main.java
+++ yandex-academy/open-lectures-2022/SubtreeSize/src/Main.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.print("Enter the number of vertices: ");
         int n = validateIntInput();
+    }
+
+    public static int validateIntInput() {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        String regex = "^[1-9]\\d*$";
+
+        if (input.trim().isEmpty()) {
+            throw new IllegalArgumentException("Input is null or empty or whitespace");
+        }
+
+        while (!input.matches(regex)) {
+            System.out.println("Input value is not positive integer or zero");
+            System.out.print("Please try again: ");
+            input = scan.next();
+        }
+        return Integer.parseInt(input);
     }
 }
