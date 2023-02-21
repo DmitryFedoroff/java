0d6f5e4c9b35f7c0cd20fd7ae93342395ac8b25a
DmitryFedoroff <fedoroffx@gmail.com>
2023-02-21 23:48:11 +0700

    feat: Store size of each subtree in array

--- yandex-academy/open-lectures-2022/SubtreeSize/src/Subtree.java
+++ yandex-academy/open-lectures-2022/SubtreeSize/src/Subtree.java
@@ -1,6 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Subtree {
+
+    public static int[] storeSubtreeSizes(int numberOfVertices) {
+        List<List<Integer>> adjacencyList = new ArrayList<>();
+
+        for (int i = 0; i <= numberOfVertices; i++) {
+            adjacencyList.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < numberOfVertices - 1; i++) {
+            System.out.print("Enter pairs of numbers (vertices connected by an edge): ");
+            Scanner input = new Scanner(System.in);
+            int vertexA = input.nextInt();
+            int vertexB = input.nextInt();
+            adjacencyList.get(vertexA).add(vertexB);
+            adjacencyList.get(vertexB).add(vertexA);
+        }
+
+        int[] subtreeSizes = new int[numberOfVertices + 1];
+        Arrays.fill(subtreeSizes, -1);
+        calculateSubtreeSizes(1, adjacencyList, subtreeSizes);
+
+        int[] result = new int[numberOfVertices];
+        for (int i = 1; i <= numberOfVertices; i++) {
+            result[i - 1] = subtreeSizes[i];
+        }
+        return result;
+    }
 
     public static int calculateSubtreeSizes(int vertex, List<List<Integer>> adjacencyList, int[] subtreeSizes) {
         subtreeSizes[vertex] = 1;
