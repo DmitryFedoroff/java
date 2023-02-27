642480d4e3ec49e0a63e7eeefed98d216e1e0168
DmitryFedoroff <fedoroffx@gmail.com>
2023-02-28 00:03:40 +0700

    feat: Check password against specific regular expression pattern then check if password and password confirmation match and if password does not meet requirements then WrongPasswordException is thrown

--- geekbrains/java-exceptions/AdvancedExceptionsHandling/src/Registration.java
+++ geekbrains/java-exceptions/AdvancedExceptionsHandling/src/Registration.java
@@ -18,4 +18,15 @@ public class Registration {
             throw new WrongLoginException();
         }
     }
+
+    private static void validatePassword(String password, String confirmPassword) throws WrongPasswordException {
+        String regex = "^[a-zA-Z0-9_]{1,19}$";
+
+        if (!password.matches(regex)) {
+            throw new WrongPasswordException("Password must contain only Latin letters, numbers and underscores, and be less than 20 characters long");
+        }
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException("Password and password confirmation do not match");
+        }
+    }
 }
