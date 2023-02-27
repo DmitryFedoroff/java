2c560f8d88ba0bc254785f9778e1a2ee22de4e13
DmitryFedoroff <fedoroffx@gmail.com>
2023-02-28 01:27:53 +0700

    dc: Print message about successful or failed registration depending on value of boolean variable

--- geekbrains/java-exceptions/AdvancedExceptionsHandling/src/Main.java
+++ geekbrains/java-exceptions/AdvancedExceptionsHandling/src/Main.java
@@ -6,5 +6,11 @@ public class Main {
         String confirmPassword = "19_LatinLettersNums";
 
         boolean isRegistered = Registration.register(login, password, confirmPassword);
+
+        if (isRegistered) {
+            System.out.println("Registration successful");
+        } else {
+            System.out.println("Registration failed");
+        }
     }
 }
