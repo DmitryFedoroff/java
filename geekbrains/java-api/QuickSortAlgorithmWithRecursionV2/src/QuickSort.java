45a299b13fe5985ec3f9eba716388de3f8c5d539
DmitryFedoroff <fedoroffx@gmail.com>
2023-03-02 19:36:58 +0700

    feat: Exchange two specific elements in defined positions

---
+++ geekbrains/java-api/QuickSortAlgorithmWithRecursionV2/src/QuickSort.java
@@ -0,0 +1,8 @@
+public class QuickSort {
+
+    static void swap(int[] array, int leftValue, int rightValue) {
+        int temp = array[leftValue];
+        array[leftValue] = array[rightValue];
+        array[rightValue] = temp;
+    }
+}
