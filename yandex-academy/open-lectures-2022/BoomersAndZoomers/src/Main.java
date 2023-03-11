9cb4e904b082513bfb519b527fc43ac2118df059
DmitryFedoroff <fedoroffx@gmail.com>
2023-03-11 20:01:22 +0700

    feat: Check if input is positive integer and if it is not throw IllegalArgumentException

--- yandex-academy/open-lectures-2022/BoomersAndZoomers/src/Main.java
+++ yandex-academy/open-lectures-2022/BoomersAndZoomers/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -6,5 +8,27 @@ public class Main {
 
         System.out.print("Age of people: ");
         int[] age = validateIntInput();
+    }
+
+    public static int[] validateIntInput() {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+
+        if (input.trim().isEmpty()) {
+            throw new IllegalArgumentException("Input is null or empty or whitespace");
+        }
+
+        String[] inputArray = input.trim().split("\\s+");
+        int[] result = new int[inputArray.length];
+        String regex = "^[1-9]\\d*$";
+
+        for (int i = 0; i < inputArray.length; i++) {
+            String inputNumber = inputArray[i];
+            if (!inputNumber.matches(regex)) {
+                throw new IllegalArgumentException("Input value is not positive integer or zero");
+            }
+            result[i] = Integer.parseInt(inputNumber);
+        }
+        return result;
     }
 }
