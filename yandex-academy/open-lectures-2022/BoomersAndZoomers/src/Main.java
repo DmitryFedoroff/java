006d8bb29cbd62420f5663f6ed1bbca35f780c12
DmitryFedoroff <fedoroffx@gmail.com>
2023-03-11 19:55:42 +0700

    dc: Initialize variable and integer array

---
+++ yandex-academy/open-lectures-2022/BoomersAndZoomers/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Number of people: ");
+        int n = validateIntInput()[0];
+
+        System.out.print("Age of people: ");
+        int[] age = validateIntInput();
+    }
+}
