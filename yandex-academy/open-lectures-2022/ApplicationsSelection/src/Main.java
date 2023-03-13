4317ab3135588d5ed81cc3886267ad5a6dddd141
DmitryFedoroff <fedoroffx@gmail.com>
2023-03-13 21:22:06 +0700

    dc: Create instances of TotalAppInputValidator and AppDescInputValidator classes and assign them to corresponding variables

---
+++ yandex-academy/open-lectures-2022/ApplicationsSelection/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        TotalAppInputValidator totalAppInputValidator = new TotalAppInputValidator();
+        AppDescInputValidator appDescInputValidator = new AppDescInputValidator();
+    }
+}
