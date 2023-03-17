8fc65b8da3dc2794a374540c762f89ab41aa35c8
DmitryFedoroff <fedoroffx@gmail.com>
2023-03-17 23:19:08 +0700

    refactor: Remove validateIntInput method and use separate classes for validating total number of vertices and number of connected vertices

--- yandex-academy/open-lectures-2022/SubtreeSize/src/Main.java
+++ yandex-academy/open-lectures-2022/SubtreeSize/src/Main.java
@@ -1,31 +1,17 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
+        TotalVertInputValidator totalVertInputValidator = new TotalVertInputValidator();
+        ConnVertInputValidator connVertInputValidator = new ConnVertInputValidator();
+
         System.out.print("Enter the number of vertices: ");
-        int n = validateIntInput();
+        int numVertices = totalVertInputValidator.validate();
 
-        int[] answer = Subtree.storeSubtreeSizes(n);
+        int[] result = Subtree.storeSubtreeSizes(numVertices, connVertInputValidator);
 
-        System.out.print("Size of subtree for each vertex: ");
-        Subtree.print(answer);
-    }
+        System.out.print("Size of subtree for each vertex: " + Subtree.print(result));
 
-    public static int validateIntInput() {
-        Scanner scan = new Scanner(System.in);
-        String input = scan.nextLine();
-        String regex = "^[1-9]\\d*$";
-
-        if (input.trim().isEmpty()) {
-            throw new IllegalArgumentException("Input is null or empty or whitespace");
-        }
-
-        while (!input.matches(regex)) {
-            System.out.println("Input value is not positive integer or zero");
-            System.out.print("Please try again: ");
-            input = scan.next();
-        }
-        return Integer.parseInt(input);
+        totalVertInputValidator.closeScanner();
+        connVertInputValidator.closeScanner();
     }
 }
