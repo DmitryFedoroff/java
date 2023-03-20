f49f8d973c9941a0b53340a6adc043142d32a9a1
DmitryFedoroff <fedoroffx@gmail.com>
2023-03-20 23:16:49 +0700

    dc: Create instances of NatNumInputValidator and DailyPriceInputValidator classes and assign them to corresponding variables then validates user input for initial investment amount, the number of days, and daily gas prices

---
+++ yandex-academy/open-lectures-2022/BuyAndSell/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        NatNumInputValidator natNumInputValidator = new NatNumInputValidator();
+
+        System.out.print("Enter initial investment: ");
+        int investment = natNumInputValidator.validate();
+
+        System.out.print("Enter number of days: ");
+        int days = natNumInputValidator.validate();
+
+        DailyPriceInputValidator dailyPriceInputValidator = new DailyPriceInputValidator(days);
+
+        System.out.print("Enter prices of gas on each day separated by space: ");
+        int[] prices = dailyPriceInputValidator.validate();
+    }
+}
