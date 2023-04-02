3d56fee4feb855972d4c6b0c99836003d7c3ff47
DmitryFedoroff <fedoroffx@gmail.com>
2023-04-02 20:13:41 +0700

    dc: Create instances of NatNumInputValidator and ArrayNumsInputValidator classes and assign them to appropriate variables and validate user input

---
+++ yandex-academy/open-lectures-2022/BrewPotions/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        NatNumInputValidator natNumInputValidator = new NatNumInputValidator();
+
+        System.out.print("Enter number of ingredients: ");
+        int numOfIngredients = natNumInputValidator.validate();
+
+        System.out.print("Enter number of potions: ");
+        int numOfPotions = natNumInputValidator.validate();
+
+        ArrayNumsInputValidator arrayNumsInputValidator = new ArrayNumsInputValidator(numOfIngredients);
+
+        System.out.print("Enter ingredients utility (higher number is better): ");
+        int[] utility = arrayNumsInputValidator.validate();
+    }
+}
