278b11d081d75df62d26b619a93062895c36a21e
DmitryFedoroff <fedoroffx@gmail.com>
2023-04-07 23:05:20 +0700

    refactor: Create instances of LatinCyrillicInputValidator and PalindromeBreaker classes and assign them to appropriate variables, remove validateStrInput method and close scanner object latinCyrillicInputValidator

--- yandex-academy/open-lectures-2022/BreakPalindrome/src/Main.java
+++ yandex-academy/open-lectures-2022/BreakPalindrome/src/Main.java
@@ -1,23 +1,16 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Enter palindrome: ");
-        String myStr = validateStrInput();
-        System.out.printf("Broken palindrome: %s", PalindromeBreaker.breakPalindrome(myStr));
-    }
-
-    public static String validateStrInput() {
-        Scanner scan = new Scanner(System.in);
+        LatinCyrillicInputValidator latinCyrillicInputValidator = new LatinCyrillicInputValidator();
 
         System.out.print("Enter palindrome: ");
-        String input = scan.nextLine();
+        String str = latinCyrillicInputValidator.validate();
 
-        if (input.trim().isEmpty()) {
-            throw new IllegalArgumentException("Input is null or empty or whitespace");
-        }
-        scan.close();
-        return input;
+        PalindromeBreaker palindromeBreaker = new PalindromeBreaker();
+        String result = palindromeBreaker.breakPalindrome(str);
+
+        System.out.println("Broken palindrome: " + result);
+
+        latinCyrillicInputValidator.closeScanner();
     }
 }
