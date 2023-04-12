979a5f7aad4813cce1296c1dc03ed9c9081f9ead
DmitryFedoroff <fedoroffx@gmail.com>
2023-04-12 18:52:58 +0700

    dc: Create instances of AbsoluteAddrInputValidator class and assign it to appropriate variable and then validate user input

---
+++ yandex-academy/open-lectures-2022/CanonicalPath/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        AbsoluteAddrInputValidator absoluteAddrInputValidator = new AbsoluteAddrInputValidator();
+
+        System.out.print("Enter absolute address: ");
+        String inputPath = absoluteAddrInputValidator.validate();
+    }
+}
