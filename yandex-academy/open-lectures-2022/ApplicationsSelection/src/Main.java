abed29c5f6eb9918b9037f85361050c46c86d0a4
DmitryFedoroff <fedoroffx@gmail.com>
2023-04-20 23:38:42 +0700

    refactor: Create instance of ApplicationFinder class and call findMaxApplications method of ApplicationFinder class instead of calling static methods directly from class

--- yandex-academy/open-lectures-2022/ApplicationsSelection/src/Main.java
+++ yandex-academy/open-lectures-2022/ApplicationsSelection/src/Main.java
@@ -7,7 +7,8 @@ public class Main {
         System.out.print("Enter number of applications: ");
         int totalApplications = totalAppInputValidator.validate();
 
-        int result = ApplicationFinder.findMaxApplications(totalApplications, appDescInputValidator);
+        ApplicationFinder applicationFinder = new ApplicationFinder();
+        int result = applicationFinder.findMaxApplications(totalApplications, appDescInputValidator);
 
         System.out.print("Max number of applications that can be fulfilled: " + result);
 
