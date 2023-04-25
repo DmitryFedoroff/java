b9a37b6bf1cee6b3728c59d936a5fb90d25c7b65
DmitryFedoroff <fedoroffx@gmail.com>
2023-04-25 22:54:35 +0700

    dc: Create instances of NatNumInputValidator and SubstancesInputValidator classes and assign them to appropriate variables and validate user input

---
+++ yandex-academy/open-lectures-2022/Rejection/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+        NatNumInputValidator natNumInputValidator = new NatNumInputValidator();
+
+        System.out.print("Enter number of substances in list of desirable substances: ");
+        int desirableSubstancesCount = natNumInputValidator.validate();
+
+        SubstancesInputValidator desirableSubstancesValidator = new SubstancesInputValidator(desirableSubstancesCount);
+
+        System.out.print("Enter list of desirable substances: ");
+        String[] desirableSubstances = desirableSubstancesValidator.validate();
+
+        System.out.print("Enter number of substances obtained in reactor: ");
+        int reactorSubstancesCount = natNumInputValidator.validate();
+
+        SubstancesInputValidator reactorSubstancesInputValidator = new SubstancesInputValidator(reactorSubstancesCount);
+
+        System.out.print("Enter list of substances obtained in reactor: ");
+        String[] reactorSubstances = reactorSubstancesInputValidator.validate();
+    }
+}
