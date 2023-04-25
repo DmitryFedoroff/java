0c4d67c1e5ce363fa21228b7331f9c2097491595
DmitryFedoroff <fedoroffx@gmail.com>
2023-04-25 23:22:06 +0700

    refactor: Remove static keyword from printResult method declaration leaving only public access modifier

--- yandex-academy/open-lectures-2022/Rejection/src/ResultPrinter.java
+++ yandex-academy/open-lectures-2022/Rejection/src/ResultPrinter.java
@@ -1,6 +1,6 @@
 public class ResultPrinter {
 
-    public static void printResult(String[] result) {
+    public void printResult(String[] result) {
         System.out.print("Result: ");
         for (String res : result) {
             System.out.print(res + " ");
