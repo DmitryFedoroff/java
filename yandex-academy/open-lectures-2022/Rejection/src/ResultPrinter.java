0b9bbdcaaa55a7d326fa6bc3af181c1d8ef77e6b
DmitryFedoroff <fedoroffx@gmail.com>
2023-04-25 22:44:37 +0700

    feat: Print array of strings as formatted result in console output

---
+++ yandex-academy/open-lectures-2022/Rejection/src/ResultPrinter.java
@@ -0,0 +1,9 @@
+public class ResultPrinter {
+
+    public static void printResult(String[] result) {
+        System.out.print("Result: ");
+        for (String res : result) {
+            System.out.print(res + " ");
+        }
+    }
+}
