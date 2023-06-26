602a10e4f74eddc8705a16027a083f6c12e5d6c4
DmitryFedoroff <fedoroffx@gmail.com>
2023-06-26 23:00:02 +0700

    feat: Process and validate array data
    #comment The ArrayProcessor class, implementing ArrayProcessorInterface, contains logic for validating array size and data, and calculates the sum of array elements
    
    Affected: ArrayProcessor

---
+++ geekbrains/java-exceptions/ExceptionHandledArrayProcessor/src/processors/ArrayProcessor.java
@@ -0,0 +1,35 @@
+package processors;
+
+import exceptions.MyArraySizeException;
+import exceptions.MyArrayDataException;
+import interfaces.ArrayProcessorInterface;
+
+public class ArrayProcessor implements ArrayProcessorInterface {
+    private static final int REQUIRED_ARRAY_SIZE = 4;
+
+    @Override
+    public int processArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        if (array.length != REQUIRED_ARRAY_SIZE) {
+            throw new MyArraySizeException("Array size is not " + REQUIRED_ARRAY_SIZE + "x" + REQUIRED_ARRAY_SIZE);
+        }
+
+        for (String[] row : array) {
+            if (row.length != REQUIRED_ARRAY_SIZE) {
+                throw new MyArraySizeException("Array size is not " + REQUIRED_ARRAY_SIZE + "x" + REQUIRED_ARRAY_SIZE);
+            }
+        }
+
+        int sum = 0;
+
+        for (int rowIndex = 0; rowIndex < array.length; rowIndex++) {
+            for (int columnIndex = 0; columnIndex < array[rowIndex].length; columnIndex++) {
+                try {
+                    sum += Integer.parseInt(array[rowIndex][columnIndex]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Invalid data in cell [" + rowIndex + "][" + columnIndex + "]");
+                }
+            }
+        }
+        return sum;
+    }
+}
