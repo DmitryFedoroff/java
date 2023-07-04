e4a9d293faba0ad4e8caf42e421f0541af31c5ca
DmitryFedoroff <fedoroffx@gmail.com>
2023-07-05 00:16:33 +0700

    feat: Implement human player move validation
    #comment Add validation for human player moves in HumanPlayer class. The TwoNaturalNumbersValidator class is used to validate user input for coordinates of the move
    
    Affected: HumanPlayer (human player move validation)

--- geekbrains/java-oop/TicTacToeAI/src/player/HumanPlayer.java
+++ geekbrains/java-oop/TicTacToeAI/src/player/HumanPlayer.java
@@ -1,20 +1,25 @@
 package player;
 
 import model.GameField;
+import validation.InputValidator;
+import validation.TwoNaturalNumbersValidator;
 import java.util.Scanner;
 
 public class HumanPlayer implements Player {
-    private Scanner scanner = new Scanner(System.in);
+    private InputValidator inputValidator;
 
     @Override
     public void makeMove(GameField field) {
         int x, y;
+        inputValidator = new TwoNaturalNumbersValidator(new Scanner(System.in));
         
         do {
             System.out.print("Enter coordinates of your turn x and y, separated by space: ");
-            x = scanner.nextInt() - 1;
-            y = scanner.nextInt() - 1;
+            int[] coordinates = inputValidator.validate();
+            x = coordinates[0] - 1;
+            y = coordinates[1] - 1;
         } while (!field.isCellValid(x, y) || !field.isCellEmpty(x, y));
         field.setCell(x, y, 'X');
     }
 }
+
