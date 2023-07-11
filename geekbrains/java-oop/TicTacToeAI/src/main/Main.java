f5a891b33f3240cd31282b3eca2fe150653382b6
DmitryFedoroff <fedoroffx@gmail.com>
2023-07-11 23:46:48 +0700

    refactor: Update Main to use dependency injection in Game
    #comment Update Main class to create instances of HumanPlayer, AIPlayer, and Scanner, and pass them into the Game constructor, thus using the newly implemented dependency injection in the Game class
    
    Affected: Main (main method)

--- geekbrains/java-oop/TicTacToeAI/src/main/Main.java
+++ geekbrains/java-oop/TicTacToeAI/src/main/Main.java
@@ -1,10 +1,14 @@
 package main;
 
 import game.Game;
+import player.AIPlayer;
+import player.HumanPlayer;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Game game = new Game();
+        Game game = new Game(new HumanPlayer(), new AIPlayer(), new Scanner(System.in));
         game.start();
     }
 }
