1c81a2b34c24198f2a2558de5094e3ebe8e2ed0f
DmitryFedoroff <fedoroffx@gmail.com>
2023-07-18 23:26:14 +0700

    refactor: Implement specific validation interfaces in validators
    #comment Updated the validator classes to implement the new specific validation interfaces. This improves the structure of the code and makes it easier to understand the role of each validator
    
    Affected: NaturalNumberValidator, TwoNaturalNumbersValidator, SymbolValidator

--- geekbrains/java-oop/TicTacToeAI/src/validation/TwoNaturalNumbersValidator.java
+++ geekbrains/java-oop/TicTacToeAI/src/validation/TwoNaturalNumbersValidator.java
@@ -2,7 +2,7 @@ package validation;
 
 import java.util.Scanner;
 
-public class TwoNaturalNumbersValidator implements InputValidator {
+public class TwoNaturalNumbersValidator implements TwoNumbersValidator {
     private static final String TWO_NATURAL_NUMBERS_REGEX = "^[1-9]\\d*\\s[1-9]\\d*$";
     private Scanner scanner;
 
@@ -11,25 +11,15 @@ public class TwoNaturalNumbersValidator implements InputValidator {
     }
 
     @Override
-    public int validateNumber() {
-        throw new UnsupportedOperationException("Method validateNumber() is not supported in this class.");
-    }
-
-    @Override
     public int[] validateNumbers() {
         while (true) {
             String input = scanner.nextLine();
             if (!input.matches(TWO_NATURAL_NUMBERS_REGEX)) {
-                System.out.print("Invalid input. Please enter two natural numbers separated by a space: ");
+                System.out.print("Invalid input. Please enter two natural numbers separated by space: ");
             } else {
                 String[] numbers = input.split("\\s");
                 return new int[]{Integer.parseInt(numbers[0]), Integer.parseInt(numbers[1])};
             }
         }
-    }
-
-    @Override
-    public String validateSymbol() {
-        throw new UnsupportedOperationException("Method validateSymbol() is not supported in this class.");
     }
 }
