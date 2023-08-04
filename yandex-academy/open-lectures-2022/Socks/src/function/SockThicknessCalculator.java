138bc49730e3d22b36afb2b400b970e94e662f07
DmitryFedoroff <fedoroffx@gmail.com>
2023-08-04 20:09:16 +0700

    refactor: Use BufferedReader for file reading
    #comment Modify SockThicknessCalculator to read the file line by line using BufferedReader instead of reading all lines at once. Update Main class to pass the file path to SockThicknessCalculator, aligning with the refactor to use BufferedReader for file reading. This change enhances memory efficiency and is particularly beneficial for large files
    
    Affected: SockThicknessCalculator, Main (file reading)

--- yandex-academy/open-lectures-2022/Socks/src/function/SockThicknessCalculator.java
+++ yandex-academy/open-lectures-2022/Socks/src/function/SockThicknessCalculator.java
@@ -1,20 +1,21 @@
 package function;
 
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class SockThicknessCalculator implements SockThicknessCalculatorInterface {
-
     private int l;
     private int n;
     private int m;
-    private List<String> data;
+    private String filePath;
     private int[] pointsOfInterest;
 
-    public SockThicknessCalculator(int l, int n, int m, List<String> data, int[] pointsOfInterest) {
+    public SockThicknessCalculator(int l, int n, int m, String filePath, int[] pointsOfInterest) {
         this.l = l;
         this.n = n;
         this.m = m;
-        this.data = data;
+        this.filePath = filePath;
         this.pointsOfInterest = pointsOfInterest;
     }
 
@@ -22,12 +23,19 @@ public class SockThicknessCalculator implements SockThicknessCalculatorInterface
     public void calculateThickness() {
         int[] balance = new int[l + 1];
 
-        for (int i = 0; i < n; i++) {
-            String[] parts = data.get(i).split("\\s+");
-            int left = Integer.parseInt(parts[0]);
-            int right = Integer.parseInt(parts[1]);
-            balance[left - 1] += 1;
-            balance[right] -= 1;
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            for (int i = 0; i < n; i++) {
+                line = reader.readLine();
+                String[] parts = line.split("\\s+");
+                int left = Integer.parseInt(parts[0]);
+                int right = Integer.parseInt(parts[1]);
+                balance[left - 1] += 1;
+                balance[right] -= 1;
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading the file: " + e.getMessage());
+            return;
         }
 
         int now = 0;
