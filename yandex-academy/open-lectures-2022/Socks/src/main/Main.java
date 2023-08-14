49129d53492a92aaf64665bfaedd03afdc66cf52
DmitryFedoroff <fedoroffx@gmail.com>
2023-08-14 21:55:50 +0700

    refactor: Improve Scanner resource management in Main class
    #comment Use try-with-resources for Scanner in Main to ensure it's automatically closed after usage. This enhances resource management and avoids potential resource leaks
    
    Affected: Main (resource management)

--- yandex-academy/open-lectures-2022/Socks/src/main/Main.java
+++ yandex-academy/open-lectures-2022/Socks/src/main/Main.java
@@ -12,43 +12,45 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
         ErrorHandlerInterface errorHandler = new BasicErrorHandler();
-
         String filePath = "socks_ends_data.txt";
-        SockFileValidatorInterface fileValidator = new SockFileValidator(filePath);
-        if (!fileValidator.isFileValid()) {
-            return;
+
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            SockFileValidatorInterface fileValidator = new SockFileValidator(filePath);
+            if (!fileValidator.isFileValid()) {
+                return;
+            }
+
+            SockThicknessCalculatorInterface calculator = new SockThicknessCalculator(0, 0, 0, filePath, null);
+
+            try {
+                calculator.validateFileData();
+            } catch (InvalidFileDataException e) {
+                errorHandler.handleException(e);
+                return;
+            }
+
+            fileValidator.notifyFileReadSuccessfully();
+
+            InputValidator inputValidator = new TableSocksPointsValidator(scanner);
+            System.out.print("Enter numbers for L, N, and M separated by whitespace: ");
+            List<Integer> inputValues = inputValidator.validate();
+            int l = inputValues.get(0);
+            int n = inputValues.get(1);
+            int m = inputValues.get(2);
+
+            GirlsInterestPointsValidatorInterface pointValidator = new GirlsInterestPointsValidator(scanner, m);
+            List<Integer> pointsOfInterest = pointValidator.validateInterestPoints();
+
+            calculator = new SockThicknessCalculator(l, n, m, filePath, pointsOfInterest);
+            try {
+                List<Integer> thickness = calculator.calculateThickness();
+                calculator.printThickness(thickness);
+            } catch (Exception e) {
+                errorHandler.handleException(e);
+            }
         }
-
-        SockThicknessCalculatorInterface calculator = new SockThicknessCalculator(0, 0, 0, filePath, null);
-
-        try {
-            calculator.validateFileData();
-        } catch (InvalidFileDataException e) {
-            errorHandler.handleException(e);
-            return;
-        }
-
-        fileValidator.notifyFileReadSuccessfully();
-
-        InputValidator inputValidator = new TableSocksPointsValidator(scanner);
-        System.out.print("Enter numbers for L, N, and M separated by whitespace: ");
-        List<Integer> inputValues = inputValidator.validate();
-        int l = inputValues.get(0);
-        int n = inputValues.get(1);
-        int m = inputValues.get(2);
-
-        GirlsInterestPointsValidatorInterface pointValidator = new GirlsInterestPointsValidator(scanner, m);
-        List<Integer> pointsOfInterest = pointValidator.validateInterestPoints();
-
-        calculator = new SockThicknessCalculator(l, n, m, filePath, pointsOfInterest);
-        try {
-            List<Integer> thickness = calculator.calculateThickness();
-            calculator.printThickness(thickness);
-        } catch (Exception e) {
-            errorHandler.handleException(e);
-        }
-        scanner.close();
     }
 }
