13f2d8845ca55844fb4bc13e58933d774c1d3247
DmitryFedoroff <fedoroffx@gmail.com>
2023-08-24 22:18:15 +0700

    refactor: Update calculator factory for new constructor params
    #comment Modify SockThicknessCalculatorFactory to accommodate the changes in SockThicknessCalculator's constructor, replacing filePath with fileLines
    
    Affected: SockThicknessCalculatorFactory (factory update)

--- yandex-academy/open-lectures-2022/Socks/src/factories/SockThicknessCalculatorFactory.java
+++ yandex-academy/open-lectures-2022/Socks/src/factories/SockThicknessCalculatorFactory.java
@@ -5,7 +5,7 @@ import function.SockThicknessCalculatorInterface;
 import java.util.List;
 
 public class SockThicknessCalculatorFactory {
-    public SockThicknessCalculatorInterface create(int l, int n, int m, String filePath, List<Integer> pointsOfInterest) {
-        return new SockThicknessCalculator(l, n, m, filePath, pointsOfInterest);
+    public SockThicknessCalculatorInterface create(int l, int n, int m, List<String> fileLines, List<Integer> pointsOfInterest) {
+        return new SockThicknessCalculator(l, n, m, fileLines, pointsOfInterest);
     }
 }
