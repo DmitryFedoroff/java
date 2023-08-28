18cee9e0dac1297cce65d18847a67385ceab102a
DmitryFedoroff <fedoroffx@gmail.com>
2023-08-28 19:33:43 +0700

    feat: Implement factory interfaces
    #comment Modify existing SockFileValidatorFactory and SockThicknessCalculatorFactory to implement the newly introduced interfaces
    
    Affected: SockFileValidatorFactory, SockThicknessCalculatorFactory (interfaces implementation)

--- yandex-academy/open-lectures-2022/Socks/src/factories/SockFileValidatorFactory.java
+++ yandex-academy/open-lectures-2022/Socks/src/factories/SockFileValidatorFactory.java
@@ -3,7 +3,8 @@ package factories;
 import validation.SockFileValidator;
 import validation.SockFileValidatorInterface;
 
-public class SockFileValidatorFactory {
+public class SockFileValidatorFactory implements FileValidatorFactoryInterface {
+    @Override
     public SockFileValidatorInterface create(String filePath) {
         return new SockFileValidator(filePath);
     }
