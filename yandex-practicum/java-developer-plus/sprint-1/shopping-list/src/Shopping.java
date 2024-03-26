8c90f9bdbc2ed68cc1d1d524cbcec2374b833d36
DmitryFedoroff <fedoroffx@gmail.com>
2024-03-26 23:43:00 +0700

    feat: Add product to shopping list
    #comment Enable users to input and add unique products to the shopping list, expanding array if necessary
    
    Affected: main

--- yandex-practicum/java-developer-plus/sprint-1/shopping-list/src/Shopping.java
+++ yandex-practicum/java-developer-plus/sprint-1/shopping-list/src/Shopping.java
@@ -20,7 +20,30 @@ public class Shopping {
             scanner.nextLine();
 
             if (actionNumber == 1) {
+                System.out.print("\nВведите название товара без пробелов, используя символ подчёркивания для разделения слов, например: \"кокосовое_молоко\": ");
+                String productName = scanner.next();
 
+                boolean isProductInList = false;
+                for (int i = 0; i < productCount; i++) {
+                    if (shoppingList[i].equals(productName)) {
+                        isProductInList = true;
+                        break;
+                    }
+                }
+
+                if (!isProductInList) {
+                    if (productCount >= shoppingList.length) {
+                        String[] expandedShoppingList = new String[shoppingList.length * 2];
+                        System.arraycopy(shoppingList, 0, expandedShoppingList, 0, shoppingList.length);
+                        shoppingList = expandedShoppingList;
+                    }
+
+                    shoppingList[productCount] = productName;
+                    productCount++;
+                    System.out.println("\nТовар \"" + productName + "\" добавлен в список под номером " + productCount + ".");
+                } else {
+                    System.out.println("\nТовар \"" + productName + "\" уже есть в списке.");
+                }
             } else if (actionNumber == 2) {
 
             } else if (actionNumber == 3) {
