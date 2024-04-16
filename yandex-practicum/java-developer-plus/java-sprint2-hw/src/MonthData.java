1d62eaa32053f45b673016bce5d46f33d4f6f0ca
DmitryFedoroff <fedoroffx@gmail.com>
2024-04-16 23:11:31 +0700

    feat: List daily steps by month
    #comment Output each day's step count for a selected month
    
    Affected: MonthData

---
+++ yandex-practicum/java-developer-plus/java-sprint2-hw/src/MonthData.java
@@ -0,0 +1,9 @@
+public class MonthData {
+    int[] days = new int[30];
+
+    void printDaysAndStepsFromMonth() {
+        for (int i = 0; i < days.length; i++) {
+            System.out.println((i + 1) + " день: " + days[i]);
+        }
+    }
+}
